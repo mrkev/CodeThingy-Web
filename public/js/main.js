34a39096fcd42434326bef9d64314e427d2baba8
Kevin <mr.kev@me.com>
2013-11-29 17:04:46 -0500

    Change firebase to codethingy.firebaseio.com

--- public/js/main.js
+++ public/js/main.js
@@ -41,8 +41,20 @@ window.onload = function() {
 
 };
 
+function getExampleRef() {
+  // Get hash from end of URL or generate a random one.
 
+  var ref = new Firebase('https://code-thingy.firebaseio.com');
+  var hash = window.location.hash.replace(/#/g, '');
+  if (hash) {
+    ref = ref.child(hash);
+  } else {
+    ref = ref.push(); // generate unique location.
+    window.location = window.location + '#' + ref.name(); // add it as a hash to the URL.
+  }
 
+  if (typeof console !== 'undefined')
+    console.log('Firebase data: ', ref.toString());
 
-
-
+  return ref;
+}
